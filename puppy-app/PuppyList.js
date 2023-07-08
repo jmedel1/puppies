ce73cbe7bb6eee1c0c352720b44f3a42e02aaa19
jael <jael.medel30@gmail.com>
2023-07-07 22:33:37 -0500

    updating name,breed,image

--- puppy-app/PuppyList.js
+++ puppy-app/PuppyList.js
@@ -1,3 +1,12 @@
 import React from "react";
 import Puppy from './Puppy';
 
+function PuppyList() {
+    const puppies = [
+        {name: 'French Bulldog', breed: 'French Bulldog Breed', image:},
+        {name: 'Boston Terrier', breed: 'Boston Terrier Breed', image:},
+        {name: 'Pekingese', breed: 'Pekingese Breed', image:},
+        {name: 'Pug', breed: 'Pug Breed', image:},
+        {name: 'Bulldog', breed: 'Bulldog Breed', image:},
+    ];
+}
