7cedba86f7b7d9231cda9f98bc02866ed8a4efb0
jael <jael.medel30@gmail.com>
2023-07-07 23:12:19 -0500

    including components in app file

--- puppy-app/src/App.js
+++ puppy-app/src/App.js
@@ -4,19 +4,23 @@ import PuppyDetails from "../PuppyDetails";
 
 
 function App() {
-  return (
-    <div>
-      <h1>Welcome to Puppy Power</h1>
-      <p>Check out these adorable puppies:</p>
-        <ul>
-          <li>French Bulldog</li>
-          <li>Boston Terrier</li>
-          <li>Pekingese</li>
-          <li>Pug</li>
-          <li> English Bulldog</li>
-        </ul>
-    </div>
-  );
+const selectedPuppy = {
+  name: 'French Bulldog',
+  breed: 'French Bulldog Breed',
+  description: 'French Bulldogs are known for their affectionate nature and adorable bat-like ears.',
+};
+
+return (
+  <div>
+    <h1>Welcome to Puppy Power!</h1>
+    <PuppyList/>
+    <h2>Selected Puppy Details</h2>
+    <PuppyDetails
+    name={selectedPuppy.name}
+    breed={selectedPuppy.breed}
+    description={selectedPuppy.description} />
+  </div>
+);
 }
 
 export default App;
