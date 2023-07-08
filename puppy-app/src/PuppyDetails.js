d17ef71e086710048cdedfabfb4f662ac7dd842d
jael <jael.medel30@gmail.com>
2023-07-08 00:42:16 -0500

    adding gif to french

--- puppy-app/src/PuppyDetails.js
+++ puppy-app/src/PuppyDetails.js
@@ -9,6 +9,7 @@ function PuppyDetails() {
         name: 'French Bulldog',
         breed: 'French Bulldog Breed',
         description: 'French Bulldogs are known for their affectionate nature and adorable bat-like ears.',
+        movingPicture: 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMGsxMXhreHFjemp1eWc4MXdoMHlqbWdyemF2OHF2ZmZoMHE0dW0xciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/dEPGknRiEniec/giphy.gif',
     };
 
     return (
@@ -17,6 +18,7 @@ function PuppyDetails() {
             <h3>{puppy.name}</h3>
             <p>Breed: {puppy.breed}</p>
             <p>{puppy.description}</p>
+            <img src={puppy.movingPicture} alt={puppy.name} />
         </div>
     )
 }
