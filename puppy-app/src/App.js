0531e730bd1134cb29aed69ac865379106198cdd
jael <jael.medel30@gmail.com>
2023-07-07 23:57:02 -0500

    update routes on app

--- puppy-app/src/App.js
+++ puppy-app/src/App.js
@@ -1,27 +1,21 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Router } from "react-router-dom";
 import Navbar from "./Navbar";
 import PuppyList from "./PuppyList";
 import PuppyDetails from "./PuppyDetails";
 
 
 function App() {
-const selectedPuppy = {
-  name: 'French Bulldog',
-  breed: 'French Bulldog Breed',
-  description: 'French Bulldogs are known for their affectionate nature and adorable bat-like ears.',
-};
-
 return (
-  <div>
-    <h1>Welcome to Puppy Power!</h1>
-    <PuppyList/>
-    <h2>Selected Puppy Details</h2>
-    <PuppyDetails
-    name={selectedPuppy.name}
-    breed={selectedPuppy.breed}
-    description={selectedPuppy.description} />
-  </div>
+  <Router>
+    <div>
+      <Navbar />
+
+      <Route path="/" exact component={Home} />
+      <Route path="/puppies" component={PuppyList} />
+      <Route path="/puppy/:id" component={PuppyDetails} />
+    </div>
+  </Router>
 );
 }
 
