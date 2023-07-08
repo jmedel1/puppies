5fd71ebfd9cda41e46f6b329e1a3dd3ccba06a45
jael <jael.medel30@gmail.com>
2023-07-07 23:32:29 -0500

    export navbar

--- puppy-app/src/Navbar.js
+++ puppy-app/src/Navbar.js
@@ -3,6 +3,17 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        
-    )
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/puppies">Puppies</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 }
+
+export default Navbar;
