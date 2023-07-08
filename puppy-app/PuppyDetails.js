697b4c6b600db6d9048ade31156e010ce83f1f8d
jael <jael.medel30@gmail.com>
2023-07-07 22:57:51 -0500

    details for each pup

--- puppy-app/PuppyDetails.js
+++ puppy-app/PuppyDetails.js
@@ -3,7 +3,9 @@ import React from "react";
 function PuppyDetails({ name, breed, description }) {
     return (
         <div>
-            
+            <h2>{name}</h2>
+            <p>Breed: {breed}</p>
+            <p>{description}</p>
         </div>
     )
 }
