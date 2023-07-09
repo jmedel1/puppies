ed10e738c4addbd500c89c3351fa3f50e6ec1668
jael <jael.medel30@gmail.com>
2023-07-08 22:16:27 -0500

    adding summary to homepage

--- puppy-app/src/App.js
+++ puppy-app/src/App.js
@@ -8,9 +8,14 @@ import './Puppies.css';
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
-      <p>Welcome to Puppy Power!</p>
+      <h2>Welcome to Puppy Power!</h2>
       <p>Discover the world of adorable puppies and find your perfect companion.</p>
+      <img src="https://media.tenor.com/sTFc7j1xRJ0AAAAC/doge-dancing-doge.gif" alt="Dancing Doge"/>
+      <p>
+        Are you looking to bring a new furry friend into your life? At Puppy Power, we offer a wide selection of adorable puppies
+        for you to explore. Whether you're seeking a playful companion or a loyal family pet, our puppies are sure to capture your heart.
+        Browse our puppy listings, learn about different breeds, and discover valuable resources to help you care for your new friend.
+      </p>
     </div>
   );
 };
